014114fab125617f5db5ada4c940fe687dd46a25
Your Name <108307350+pjduri@users.noreply.github.com>
2024-06-06 00:51:49 -0500

    user signup completed

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay6;
+
+public class UserController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost("/user")]
+    public IActionResult SubmitAddUserForm(User newUser, string verify)
+    {
+        if (newUser.Password != verify)
+        {
+            ViewBag.error = "Passwords must match";
+            return View("/users/add");
+        }
+
+        ViewBag.user = newUser;
+        return View("Index");
+    }
+}
+
+// public class UserController : Controller
+// {
+//     // GET: /<controller>/
+//     public IActionResult Index()
+//     {
+//         return View();
+//     }
+
+//     // Render the form
+//     public IActionResult Add()
+//     {
+//         return View();
+//     }
+
+//     [HttpPost]
+//     [Route("/user")]
+//     public IActionResult SubmitAddUserForm(User newUser, string verify)
+//     {
+//         // add form submission handling code here
+//         if (newUser.Password == verify)
+//         {
+//             // Students may pass in just in the name of the user to the Index view.
+//             // They only need to pass in the whole object to complete the bonus mission.
+//             ViewBag.user = newUser;
+//             return View("Index");
+//         }
+//         else
+//         {
+//             ViewBag.error = "Passwords do not match! Try again!";
+//             ViewBag.userName = newUser.Username;
+//             ViewBag.eMail = newUser.Email;
+//             return View("Add");
+//         }
+//     }
+// }
---
+++ SpaDay6/Models/User.cs
@@ -0,0 +1,37 @@
+﻿namespace SpaDay6;
+
+public class User
+{
+
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public User(){  }
+
+    public User(string username, string email, string password)
+    {
+        Username = username;
+        Email = email;
+        Password = password;
+    }
+    // public string Username { get; set; }
+    // public string Email { get; set; }
+    // public string Password { get; set; }
+
+    // // Bonus Mission
+    // public DateTime SignUpTime { get; }
+
+    // public User()
+    // {
+    //     SignUpTime = DateTime.Now;
+    // }
+
+    // public User(string u, string e, string p)
+    // {
+    //     Username = u;
+    //     Email = e;
+    //     Password = p;
+    //     SignUpTime = DateTime.Now;
+    // }
+}
